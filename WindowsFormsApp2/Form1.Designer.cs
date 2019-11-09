1b1add84f1ebb21fc604a8bb5ab2fab641e46b01
Armin Bašić <basic.armin1@gmail.com>
2019-11-09 15:17:58 +0100

    commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enter = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Word = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Finish = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Reset = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.repeatLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(354, 45);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(112, 32);
+            this.Enter.TabIndex = 0;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Word
+            // 
+            this.Word.AutoSize = true;
+            this.Word.Location = new System.Drawing.Point(33, 52);
+            this.Word.Name = "Word";
+            this.Word.Size = new System.Drawing.Size(33, 13);
+            this.Word.TabIndex = 2;
+            this.Word.Text = "Word";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(107, 123);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 156);
+            this.panel1.TabIndex = 3;
+            // 
+            // Finish
+            // 
+            this.Finish.Location = new System.Drawing.Point(456, 123);
+            this.Finish.Name = "Finish";
+            this.Finish.Size = new System.Drawing.Size(75, 23);
+            this.Finish.TabIndex = 4;
+            this.Finish.Text = "Finish";
+            this.Finish.UseVisualStyleBackColor = true;
+            this.Finish.Click += new System.EventHandler(this.Finish_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(107, 316);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(307, 100);
+            this.panel2.TabIndex = 5;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(562, 123);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 6;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(456, 191);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Player1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(456, 214);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "Player2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Enabled = false;
+            this.radioButton3.Location = new System.Drawing.Point(456, 238);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.Text = "Player3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Enabled = false;
+            this.radioButton4.Location = new System.Drawing.Point(456, 261);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(60, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.Text = "Player4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Enabled = false;
+            this.radioButton5.Location = new System.Drawing.Point(456, 284);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(60, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.Text = "Player5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // repeatLabel
+            // 
+            this.repeatLabel.AutoSize = true;
+            this.repeatLabel.Location = new System.Drawing.Point(13, 132);
+            this.repeatLabel.Name = "repeatLabel";
+            this.repeatLabel.Size = new System.Drawing.Size(0, 13);
+            this.repeatLabel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 450);
+            this.Controls.Add(this.repeatLabel);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Finish);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Word);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Enter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Word;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Finish;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label repeatLabel;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Label l = new Label();
+        Label res = new Label();
+        List<string> list = new List<string>();
+        List<string> listTotal = new List<string>();
+
+
+        List<string> listPlayer1 = new List<string>();
+        List<string> listPlayer2 = new List<string>();
+        List<string> listPlayer3 = new List<string>();
+        List<string> listPlayer4 = new List<string>();
+        List<string> listPlayer5 = new List<string>();
+                        
+        int resultPlayer1 = 0;
+        int resultPlayer2 = 0;
+        int resultPlayer3 = 0;
+        int resultPlayer4 = 0;
+        int resultPlayer5 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            l.Text += "Player 1: \n";
+
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            l.Size = new Size(307, 156);
+
+            bool repeat = false;
+            for (int i=0; i<listTotal.Count; i++)
+            {
+               
+                if (listTotal[i].ToString() == textBox1.Text.ToString())
+                {
+                    repeat = true;
+                }
+                
+            }
+
+            if (!repeat) {
+            repeatLabel.Text = "";
+            list.Add(textBox1.Text);
+            listTotal.Add(textBox1.Text);
+            l.Text += textBox1.Text + Environment.NewLine;
+            textBox1.Text = string.Empty;
+            panel1.Controls.Add(l);
+            }
+            else
+            {
+                repeatLabel.Text = "Repeat!";
+            }
+            
+        }
+        
+
+        private void Finish_Click(object sender, EventArgs e)
+        {
+
+            Enter.Enabled = false;
+
+            radioButton5.Enabled = false;
+            radioButton5.Checked = false;
+
+            resultPlayer5 = GetResult(listPlayer5);
+
+            res.Size = new Size(307, 100);
+            res.Text =
+                "Player 1 result is: " + resultPlayer1
+                + "\n Player 2 result is: " + resultPlayer2
+                + "\n Player 3 result is: " + resultPlayer3
+                + "\n Player 4 result is: " + resultPlayer4
+                + "\n Player 5 result is: " + resultPlayer5 ;
+
+            panel2.Controls.Add(res);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            l.Text = "";
+            res.Text = "";
+            Enter.Enabled = true;
+
+            resultPlayer1 = 0;
+            resultPlayer2 = 0;
+            resultPlayer3 = 0;
+            resultPlayer4 = 0;
+            resultPlayer5 = 0;
+
+            listPlayer1.Clear();
+            listPlayer2.Clear();
+            listPlayer3.Clear();
+            listPlayer4.Clear();
+            listPlayer5.Clear();
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked) { 
+            radioButton1.Checked = false;
+            radioButton1.Enabled = false;
+            radioButton3.Enabled = true;
+            
+            listPlayer1 = list;
+            resultPlayer1 = GetResult(listPlayer1);
+
+                l.Text += "Player 2: \n";
+                foreach (var item in list)
+                {
+
+                    listTotal.Add(item);
+                }
+
+
+            list.Clear();
+}
+
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                radioButton2.Checked = false;
+                radioButton2.Enabled = false;
+                radioButton4.Enabled = true;
+                l.Text += "Player 3: \n";
+
+                listPlayer2 = list;
+                resultPlayer2 = GetResult(listPlayer2);
+                foreach (var item in list)
+                {
+                    listTotal.Add(item);
+                }
+                list.Clear();
+
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                radioButton3.Checked = false;
+                radioButton3.Enabled = false;
+                radioButton5.Enabled = true;
+                l.Text += "Player 4: \n";
+
+                listPlayer3 = list;
+                resultPlayer3 = GetResult(listPlayer3);
+                foreach (var item in list)
+                {
+                    listTotal.Add(item);
+                }
+                list.Clear();
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked)
+            {
+                radioButton4.Checked = false;
+                radioButton4.Enabled = false;
+
+                l.Text += "Player 5: \n";
+                listPlayer4 = list;
+                resultPlayer4 = GetResult(listPlayer4);
+                foreach (var item in list)
+                {
+                    listTotal.Add(item);
+                }
+                list.Clear();
+
+            }
+        }
+
+        private int GetResult (List<string> wordslist)
+        {
+            int playerresult=0;
+            for (int i = 0; i < wordslist.Count; i++)
+            {
+                if (list[i].Length == 3 || list[i].Length == 4)
+                {
+                    playerresult = playerresult + 1;
+                }
+                else if (list[i].Length == 5)
+                {
+                    playerresult = playerresult + 2;
+                }
+                else if (list[i].Length == 6)
+                {
+                    playerresult = playerresult + 3;
+                }
+                else if (list[i].Length == 7)
+                {
+                    playerresult = playerresult + 5;
+                }
+                else if (list[i].Length >= 8)
+                {
+                    playerresult = playerresult + 11;
+                }
+
+
+               
+
+            }
+            return playerresult;
+        }
+        
+    }
+}
